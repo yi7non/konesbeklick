40f698944c8c069ac67bcb362d38f97339a303e4
yinon <yi7non@gmail.com>
2019-12-06 16:03:38 +0200

    add countdown title and date

--- inc/countdown-title.php
+++ inc/countdown-title.php
@@ -1,6 +1,6 @@
 <?php 
 function yoyo_countdownTitle() {
-    // echo "<h2 class='yoyo-countdownTitle'>מכירת הפריט תיסגר בעוד:</h2>";
+    
     global $post;
     $id = $post->ID;
     $end_time = get_post_meta($id ,'woo_ua_auction_end_date', true);
@@ -9,88 +9,100 @@ function yoyo_countdownTitle() {
     $left_second = fmod($end_second, 60);
     $end_hours = floor($end_minute / 60) - 2;
     $left_minute = fmod($end_minute, 60);
-
- 
     ?>
-    <div id="codpen" data-id="<?php echo $id; ?>" class="wrap">  
+    <div class="open-auction-time">
+        <div class="open-auction-time__info">
+            <h3 class="open-auction-time__title">המכרז יפתח בתאריך</h3>
+            <div class="open-auction-time__content">
+                <?php
+                $start_date = explode(" ", get_post_meta($id, 'woo_ua_auction_start_date', true)); 
+                ?>
+                <time><?php echo $start_date[0]; ?></time>
+                <span>בשעה</span>
+                <time><?php echo $start_date[1]; ?></time>
+            </div>
+        </div>
 
-        <div class="countdown">
-            <div class="bloc-time hours" data-init-value="<?php echo $end_hours; ?>">
-            <span class="count-title">Hours</span>
+        <div id="codpen" data-id="<?php echo $id; ?>" class="wrap">  
 
-            <div class="figure hours hours-1">
+            <div class="countdown">
+                <div class="bloc-time hours" data-init-value="<?php echo $end_hours; ?>">
+                <span class="count-title">שעות</span>
+
+                <div class="figure hours hours-1">
+                    <span class="top">0</span>
+                    <span class="top-back">
+                    <span>0</span>
+                    </span>
+                    <span class="bottom">0</span>
+                    <span class="bottom-back">
+                    <span>0</span>
+                    </span>
+                </div>
+
+                <div class="figure hours hours-2">
+                    <span class="top">0</span>
+                    <span class="top-back">
+                    <span>0</span>
+                    </span>
+                    <span class="bottom">0</span>
+                    <span class="bottom-back">
+                    <span>0</span>
+                    </span>
+                </div>
+                </div>
+
+                <div class="bloc-time min" data-init-value="<?php echo $left_minute; ?>">
+                <span class="count-title">דקות</span>
+
+                <div class="figure min min-1">
+                    <span class="top">0</span>
+                    <span class="top-back">
+                    <span>0</span>
+                    </span>
+                    <span class="bottom">0</span>
+                    <span class="bottom-back">
+                    <span>0</span>
+                    </span>        
+                </div>
+
+                <div class="figure min min-2">
                 <span class="top">0</span>
-                <span class="top-back">
-                <span>0</span>
-                </span>
-                <span class="bottom">0</span>
-                <span class="bottom-back">
-                <span>0</span>
-                </span>
-            </div>
+                    <span class="top-back">
+                    <span>0</span>
+                    </span>
+                    <span class="bottom">0</span>
+                    <span class="bottom-back">
+                    <span>0</span>
+                    </span>
+                </div>
+                </div>
 
-            <div class="figure hours hours-2">
-                <span class="top">0</span>
-                <span class="top-back">
-                <span>0</span>
-                </span>
-                <span class="bottom">0</span>
-                <span class="bottom-back">
-                <span>0</span>
-                </span>
-            </div>
-            </div>
+                <div class="bloc-time sec" data-init-value="<?php echo $left_second; ?>">
+                <span class="count-title">שניות</span>
 
-            <div class="bloc-time min" data-init-value="<?php echo $left_minute; ?>">
-            <span class="count-title">Minutes</span>
+                    <div class="figure sec sec-1">
+                    <span class="top">0</span>
+                    <span class="top-back">
+                    <span>0</span>
+                    </span>
+                    <span class="bottom">0</span>
+                    <span class="bottom-back">
+                    <span>0</span>
+                    </span>          
+                </div>
 
-            <div class="figure min min-1">
-                <span class="top">0</span>
-                <span class="top-back">
-                <span>0</span>
-                </span>
-                <span class="bottom">0</span>
-                <span class="bottom-back">
-                <span>0</span>
-                </span>        
-            </div>
-
-            <div class="figure min min-2">
-            <span class="top">0</span>
-                <span class="top-back">
-                <span>0</span>
-                </span>
-                <span class="bottom">0</span>
-                <span class="bottom-back">
-                <span>0</span>
-                </span>
-            </div>
-            </div>
-
-            <div class="bloc-time sec" data-init-value="<?php echo $left_second; ?>">
-            <span class="count-title">Seconds</span>
-
-                <div class="figure sec sec-1">
-                <span class="top">0</span>
-                <span class="top-back">
-                <span>0</span>
-                </span>
-                <span class="bottom">0</span>
-                <span class="bottom-back">
-                <span>0</span>
-                </span>          
-            </div>
-
-            <div class="figure sec sec-2">
-                <span class="top">0</span>
-                <span class="top-back">
-                <span>0</span>
-                </span>
-                <span class="bottom">0</span>
-                <span class="bottom-back">
-                <span>0</span>
-                </span>
-            </div>
+                <div class="figure sec sec-2">
+                    <span class="top">0</span>
+                    <span class="top-back">
+                    <span>0</span>
+                    </span>
+                    <span class="bottom">0</span>
+                    <span class="bottom-back">
+                    <span>0</span>
+                    </span>
+                </div>
+                </div>
             </div>
         </div>
     </div>
