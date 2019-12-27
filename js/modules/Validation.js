0de12c91a3da9f3d8f2942805ad6b1a8f6a45c14
yinon <yi7non@gmail.com>
2019-12-27 13:14:01 +0200

    prepare to new css

--- js/modules/Validation.js
+++ js/modules/Validation.js
@@ -4,6 +4,9 @@ export default class Validation {
     constructor() {
         this.fullname = $('.reg__field-section #name');
         this.email = $('.reg__field-section #email');
+        this.tel = $('.reg__field-section #tel');
+        this.pass = $('.reg__field-section #pass');
+        this.pass2 = $('.reg__field-section #pass2');
         this.status = {
             success: false,
             alert: false
@@ -14,6 +17,9 @@ export default class Validation {
     events() {
         this.fullname.on("input", this.validatFN.bind(this));
         this.email.on("input", this.validatEmail.bind(this));
+        this.tel.on("input", this.validatTel.bind(this));
+        this.pass.on("input", this.validatPass.bind(this));
+        this.pass2.on("input", this.validatPass2.bind(this));
     }
 
     validatFN() {
@@ -40,6 +46,56 @@ export default class Validation {
         }
 
         this.generalValidate(this.email, text, condition, "שדה המייל צריך להכיל @ ולאחריו שם חברת המייל");
+    }
+
+    validatTel() {
+        const text = this.tel.val();
+
+        const condition = () => {
+
+            if(text.length < 9) {
+                return true
+            } else {
+                return false;
+            }
+        }
+        this.generalValidate(this.tel, text, condition, "יש לרשום לפחות 9 ספרות");
+    }
+
+    validatPass() {
+        const text = this.pass.val();
+      
+        const condition = () => {
+            if(!this.lettersAndNumbers(text) || this.minNumInPass(text)) {
+                return true;
+            } else {
+                return false
+            }
+        }
+        this.generalValidate(this.pass, text, condition, "יש לכתוב לפחות 6 תווים המשולבים מאותיות ומספרים");
+    }
+
+    minNumInPass(text) {
+        return text.length < 6;
+    }
+
+    lettersAndNumbers(string) {
+        const num = /[a-zA-Z]+/g;
+        const lett = /[0-9]+/g
+        return num.test(string) && lett.test(string);
+    }
+
+    validatPass2() {
+        const text = this.pass2.val();
+      
+        const condition = () => {
+            if(text !== this.pass.val()) {
+                return true;
+            } else {
+                return false
+            }
+        }
+        this.generalValidate(this.pass2, text, condition, "דרושה התאמה מלאה בין הסיסמאות");
     }
 
     generalValidate(element ,input, condition, validatText) {
